789d34d06fd03c196e43a541863844798ed9b569
milhamap <ilhamap45@gmail.com>
2023-05-28 20:17:40 +0700

    update: cors endpoint gets school places

--- src/resolvers/school_places/index.js
+++ src/resolvers/school_places/index.js
@@ -52,7 +52,7 @@ module.exports = {
     getsSchoolPlace: async (req, res) => {
         const name = req.query.name
         try {
-            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+            res.setHeader('Access-Control-Allow-Origin', '*');
             const list = knex('list_schools')
             !name ? list : list.where('name', 'like', `%${name}%`)
             const list_schools = await list
