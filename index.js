062fb1d2d03180cb94ceff691737f261236b7cd4
milhamap <ilhamap45@gmail.com>
2023-05-28 20:03:33 +0700

    update: delete cors policy library

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-// const cors = require('cors')
 
 const app = express()
 
@@ -9,7 +8,6 @@ const schoolPlaceRouter = require('./src/routers/school_places')
 const majorRouter = require('./src/routers/majors')
 const markerRouter = require('./src/routers/markers')
 
-// app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
